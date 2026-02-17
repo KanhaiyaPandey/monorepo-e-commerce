f744a80f7896d731534a1afacbef1b6cc2c396a4
KanhaiyaPandey <kanhaiyapandey2232@gmail.com>
2026-02-17 21:09:23 +0530

    bug fix

--- apps/auth-service/src/controllers/auth.controller.ts
+++ apps/auth-service/src/controllers/auth.controller.ts
@@ -1,34 +1,32 @@
-import { NextFunction, Response, Request } from "express";
-import { checkOtpRestrictions,
-          sendOtp,
-          trackFailedOtpAttempts, trackOptRequests,
-          validateUserRegistrationInput } from "../utils/auth.helper";
-import prisma from "../../../../packages/libs/prisma";
-import { ValidationError } from "../../../../packages/error-handler";
+import { NextFunction, Response, Request } from 'express';
+import {
+  checkOtpRestrictions,
+  sendOtp,
+  trackFailedOtpAttempts,
+  trackOptRequests,
+  validateUserRegistrationInput,
+} from '../utils/auth.helper';
+import { ValidationError } from '../../../../packages/error-handler';
 
-export const userRegistration = async (req: Request, res: Response, next: NextFunction) => {
+export const userRegistration = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
   try {
     const { name, email } = validateUserRegistrationInput(req.body);
-    const existingUser = await prisma.user.findUnique({ where: { email } });
-
+    const existingUser = null;
     if (existingUser) {
-      return next(new ValidationError("User already exists with this email"));
+      return next(new ValidationError('User already exists with this email'));
     }
-
     await checkOtpRestrictions(email, next);
     await trackOptRequests(email, next);
     await trackFailedOtpAttempts(email, next);
-    await sendOtp(email, name, "user-activation-mail");
+    await sendOtp(email, name, 'user-activation-mail');
 
-    // const newUser = await prisma.user.create({
-    //   data: {
-    //     name,
-    //     email,
-    //     password: hashedPassword,
-    //   },
-    // });
+    // user creation removed; previously used Prisma client
 
-    res.status(200).json({ message: "OTP sent successfully" });
+    res.status(200).json({ message: 'OTP sent successfully' });
   } catch (error) {
     return next(error);
   }
